e518aa1ed748074506efccfbfad21d305e239693
sekihan02 <sekihan0291@gmail.com>
2022-06-08 22:34:45 +0900

    Dockerfile や起動バッチファイルの更新

--- analysis_env/docker_start.bat
+++ analysis_env/docker_start.bat
@@ -1,2 +1,3 @@
 @REM 2回目以降の起動時
-docker run -p 8888:8888 --rm --gpus all -v C:\Users\akaha\Documents\docker\workspace\analysis_env\work:/work -w /work --name my_env analysis_env
+@REM docker run -p 8888:8888 --rm --gpus all --shm-size=2gb  -v C:\Users\Public\Documents\github\Docker_env\analysis_env\work:/work -w /work --name my_env analysis_env
+docker run -p 8888:8888 --rm --gpus all --shm-size=32gb  -v C:\Users\Public\Documents\github\Docker_env\analysis_env\work:/work -w /work --name my_env analysis_env
--- analysis_env/docker_start_cmd.bat
+++ analysis_env/docker_start_cmd.bat
@@ -1,2 +1,2 @@
 @REM 起動
-docker run -it --gpus all --rm -v C:\Users\akaha\Documents\docker\workspace\analysis_env\work:/work -w /work --name my_env analysis_env bash
+docker run -it --rm --gpus all --shm-size=32gb  -v C:\Users\Public\Documents\github\Docker_env\analysis_env\work:/work -w /work --name my_env analysis_env bash
