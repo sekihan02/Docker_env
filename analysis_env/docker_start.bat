1e16a67fcbbd586e441203ffb8f140b8360575b3
sekihan02 <sekihan0291@gmail.com>
2022-10-11 00:24:43 +0900

    動的サイトに対するseleniumのdocker環境の構築内容の追加

--- analysis_env/docker_start.bat
+++ analysis_env/docker_start.bat
@@ -1,3 +1,4 @@
 @REM 2回目以降の起動時
 @REM docker run -p 8888:8888 --rm --gpus all --shm-size=2gb  -v C:\Users\Public\Documents\github\Docker_env\analysis_env\work:/work -w /work --name my_env analysis_env
-docker run -p 8888:8888 --rm --gpus all --shm-size=32gb  -v C:\Users\Public\Documents\github\Docker_env\analysis_env\work:/work -w /work --name my_env analysis_env
+docker run -p 8888:8888 --rm --gpus all --shm-size=32gb -v C:\Users\Public\Documents\github\Docker_env\analysis_env\work:/work -w /work --name my_env analysis_env
+@REM docker run -p 8888:8888 --rm --gpus all --shm-size=32gb  -v F:\:/work -w /work --name my_env analysis_env
